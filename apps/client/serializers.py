c30c7787bf3073c3ce0a143a690972166f853283
NerdPraise <praiseajayi2@gmail.com>
2023-02-19 14:39:17 +0100

    DAP-7: Create Admin group for lender's user

--- apps/client/serializers.py
+++ apps/client/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from rest_framework import serializers
 
 from apps.accounts.models import Lender, LenderPackageMap, Package
@@ -18,13 +19,15 @@ class CreateLenderSerializer(BaseLenderSerializer):
 
     def create(self, validated_data):
         validated_user_obj = validated_data.pop("user")
-        lender_company = super().create({**validated_data})
+        lender = super().create({**validated_data})
         user = User.objects.create_user(**validated_user_obj, role=User.LENDER)
+        # Assign user 'Lender's Admin' permission
+        user.groups.add(Group.objects.get(name="Lender's Admin"))
         default_package = Package.objects.first()
         p_map = LenderPackageMap.objects.create(
-            lender=lender_company,
+            lender=lender,
             package=default_package,
         )
         p_map.users.add(user)
 
-        return lender_company
+        return lender
