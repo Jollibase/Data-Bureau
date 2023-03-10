e42f1cb3dad6f1da0753886d744ab9545e83fa28
NerdPraise <praiseajayi2@gmail.com>
2023-03-10 17:17:27 +0000

    DAP-14: Add react typescript docker

---
+++ SPA/webpack.config.ts
@@ -0,0 +1,33 @@
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: './dist/bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'stylus-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}
