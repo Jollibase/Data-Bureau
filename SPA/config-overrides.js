c9266eb587912b9a3e7c7e9400265339acbfe65d
NerdPraise <praiseajayi2@gmail.com>
2023-04-02 19:30:18 +0100

    DAB-21: implement error page

--- SPA/config-overrides.js
+++ SPA/config-overrides.js
@@ -9,6 +9,7 @@ const ROOT_PATH = path.resolve(__dirname)
 const SRC_PATH = path.resolve(ROOT_PATH, './src')
 const ASSETS_PATH = path.resolve(SRC_PATH, 'assets')
 const MODULE_PATH = path.resolve(SRC_PATH, 'modules')
+const COMPONENT_PATH = path.resolve(SRC_PATH, 'components/index')
 const IMG_PATH = path.resolve(ASSETS_PATH, 'img')
 
 module.exports = override(
@@ -69,5 +70,6 @@ module.exports = override(
     '@Images': IMG_PATH,
     '@Assets': ASSETS_PATH,
     '@Modules': MODULE_PATH,
+    '@Components': COMPONENT_PATH,
   }),
 )
---
+++ SPA/src/routes/constants.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  Products: '/products',
+  Pricing: '/pricing',
+  Resources: '/resources',
+  'About Us': '/about-us',
+  Blog: '/blog',
+}
